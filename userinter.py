05027631a4e472e5fffb50eb73ccc3f23c217bd6
SterlingHi <sterlinghisey@yahoo.com>
2021-06-23 14:23:21 -0500

    added the rest of the interface methods

--- userinter.py
+++ userinter.py
@@ -13,3 +13,24 @@ class User_Interface:
     def get_user_input_number(prompt):
         user_input = input(prompt)
         return user_input
+
+    @staticmethod
+    def display_contestant_info(contestant):
+        pass
+
+    @staticmethod
+    def display_sweepstakes_info(sweepstakes):
+        pass
+
+    @staticmethod
+    def display_sweepstakes_selection_menu(all_sweepstakes):
+        pass
+
+    @staticmethod
+    def display_marketing_firm_menu_options(marketing_firm_name):
+        pass
+
+    @staticmethod
+    def display_sweepstakes_menu_options(sweepstakes_name):
+        pass
+
